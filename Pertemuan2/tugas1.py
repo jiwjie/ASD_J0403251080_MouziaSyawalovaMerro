232dfd2f509922c6a8fd22c8b83c690f408df6b4
jiwjie <mouziasyawalo@gmail.com>
2026-02-07 23:02:24 +0700

    update commentd

--- Pertemuan2/tugas1.py
+++ Pertemuan2/tugas1.py
@@ -1,7 +1,7 @@
 #==========================================================
 # TUGAS HANDS-ON MODUL 1
 # Studi Kasus: Sistem Stok Barang Kantin (Berbasis File .txt)
-
+#
 # Nama  : Mouzia Syawalova Merro
 # NIM   : J0403251080
 # Kelas : TPL B2
@@ -18,42 +18,50 @@ NAMA_FILE  = "stok_barang.txt"
 #----------------------------------------------------------
 
 def baca_stok(NAMA_FILE):
-    stock_dict = {}
+    stock_dict = {}   #inisialisasi data dictionary kosong
     with open ("stok_barang.txt", "r", encoding="utf-8") as file:
         for baris in file:
-            baris = baris.strip()
+            baris = baris.strip()   #menghilangkan karakter baris baru (\n) di akhir kalimat
             
-            data_barang = baris.split(",")
-            if len(data_barang) != 3:
+            data_barang = baris.split(",")   #memisahkan string berdasarkan koma menjadi list
+            if len(data_barang) != 3:        #jika baris tidak memiliki 3 kolom (kode, nama barang, stok), maka dilewati
                 continue
             
             kode, namaBarang, stok = data_barang 
-            stok_int = int(stok)
-            stock_dict[kode] = {"barang": namaBarang, "stok": stok_int}
-            kode, namaBarang, stok = baris.split(",") 
+            stok_int = int(stok)    #Konversi stok dari string ke integer agar bisa dihitung
+            stock_dict[kode] = {"barang": namaBarang, "stok": stok_int}    #simpan data barang ke nested dictionary dengan key kode
+            kode, namaBarang, stok = baris.split(",")   #Melakukan pemecahan ulang
             
             
     return stock_dict
-    
+
+#Memanggil fungsi baca_stok dan menyimpannya ke variabel global
 bukaData = baca_stok(NAMA_FILE)
+
+
 
 #----------------------------------------------------------
 # Fungsi : Menampilkan semua data
 #----------------------------------------------------------
 def tampilkan_barang(stock_dict):
     
+    #Cek apakah dictionary kosong
     if len(stock_dict) == 0 :
-        print ("Barang Kosong")
+        print ("Stok barang kosong.")
         return
     
+    #membuat Header Tabel
     print ("\n======= Daftar Stok Barang =======")
     print(f"{'Kode': <10} | {'Barang': <14} | {'Stok': >5}")
-    print("-" * 33)
+    print("-" * 33)    #membuat strip 33 kali (garis header)
     
+    #Looping data yang sudah diurutkan berdasarkan key (kode barang)
     for kode in sorted(stock_dict):
         namaBarang = stock_dict[kode]["barang"]
         stok = stock_dict[kode]["stok"]
         print(f"{kode: <10} | {namaBarang: <14} | {stok: >5}")
+
+
 
 #----------------------------------------------------------
 # Fungsi : Cari barang berdasarkan kode
@@ -61,10 +69,12 @@ def tampilkan_barang(stock_dict):
 
 def cari_barang(stock_dict):
     
+    #mencari barang berdasarkan kode barang
     kode_cari = input("Masukkan kode barang yang ingin dicari : ").strip()
     
+    #Cek apakah key (kode_cari) ada di dalam dictionary
     if kode_cari in stock_dict:
-        namaBarang = stock_dict[kode_cari]["barang"].strip()
+        namaBarang = stock_dict[kode_cari]["barang"].strip()     #Mengambil data dan melakukan .strip() pada nama barang
         stok = stock_dict[kode_cari]["stok"]
         
         print("\n=== Barang Ditemukan ===")
@@ -75,6 +85,8 @@ def cari_barang(stock_dict):
     else:
         print("\nBarang Tidak Ditemukan.")
 
+
+
 #----------------------------------------------------------
 # Fungsi : Tambah barang baru
 #----------------------------------------------------------
@@ -83,18 +95,23 @@ def tambah_barang(stock_dict):
     print("\n=== Tambah Barang Baru ===")
     kode = input("Masukkan Kode Barang : ").strip()
     
+    #Validasi agar tidak ada kode barang ganda
     if kode in stock_dict:
         print(f"Gagal! Kode {kode} sudah digunakan oleh barang: {stock_dict[kode]['barang']} ")
         
     else:
+        #Input nama dan jumlah stok untuk barang baru
         namaBarang = input("Masukkan Nama Barang : ").strip()
         stok = input("Masukkan Jumlah Stok Barang : ").strip()
         
+        #Menyimpan data baru ke dictionary
         stock_dict[kode] = {
             "barang" : namaBarang,
             "stok" : stok
         }
         print(f"Berhasil! {namaBarang} telah ditambahkan.")
+
+
 
 #----------------------------------------------------------
 # Fungsi  : Update Stok Barang
@@ -102,12 +119,17 @@ def tambah_barang(stock_dict):
 
 def update_stok(stock_dict):
     
+    print("\n==== Update Stock Barang ====")
+    
+    #cari kode barang yang akan di update jumlah stoknya
     kodeBarang = input("Masukkan kode barang yang ingin diupdate : ").strip()
     
+    #Cek keberadaan barang
     if kodeBarang not in stock_dict:
         print ("\nKode Barang tidak ditemukan. Update dibatalkan.")
         return
-        
+    
+    #Pilih jenis update (apakah dikurangi atau ditambah dari stok tersedia)
     print("Pilih jenis update :")
     print("1. Tambah Stok")
     print("2. Kurangi Stok")
@@ -119,38 +141,47 @@ def update_stok(stock_dict):
         return
     
     try:
+        #Input jumlah perubahan stok
         jumlah = int(input("Masukkan jumlah stok : "))
-        if jumlah < 0:
+        
+        if jumlah < 0:             #Stok tidak boleh negatif
             print("Jumlah harus berupa angka positif.")
             return
         
-        stokLama = stock_dict[kodeBarang]['stok']
+        stokLama = stock_dict[kodeBarang]['stok']          #Mengambil nilai stok saat ini sebelum diupdate
         
+        #Logika perhitungan
         if pilihan == "1":
             stokBaru = stokLama + jumlah
         elif pilihan == "2":
+            #agar stok tidak menjadi minus setelah dikurangi
             if jumlah > stokLama: 
                 print(f"Stok tidak mencukupi! Stok saat ini {stokLama}")
             stokBaru = stokLama - jumlah
         
     except ValueError:
-        print("Stok harus berupa angka. Update dibatalkan.")
+        print("Stok harus berupa angka. Update dibatalkan.")   #jika user memasukkan karakter selain angka pada input
         return
-        
+    
+    #menyimpan hasil stok baru ke dictionary
     stock_dict[kodeBarang]["stok"] = stokBaru
     
     print(f"Update berhasil. Stok barang berubah dari {stokLama} menjadi {stokBaru}")
 
+
+
 #----------------------------------------------------------
-# Fungsi : Menyimpsn perubahan ke file
+# Fungsi : Menyimpan perubahan ke file
 #----------------------------------------------------------
 
 def simpanUpdate(NAMA_FILE, stock_dict):
     with open(NAMA_FILE, "w", encoding="utf-8") as file:
-        for kode in sorted(stock_dict.keys()):
+        for kode in sorted(stock_dict.keys()):             #Mengurutkan kode
             namaBarang = stock_dict[kode]["barang"]
             stok = stock_dict[kode]["stok"]
-            file.write(f"{kode}, {namaBarang}, {stok}\n")
+            file.write(f"{kode}, {namaBarang}, {stok}\n")    #Menuliskan kembali data ke file .txt
+
+
 
 #----------------------------------------------------------
 # Program Utama
@@ -158,8 +189,10 @@ def simpanUpdate(NAMA_FILE, stock_dict):
 
 def main():
 
+    #Load data awal ke dalam variabel
     bukaData = baca_stok (NAMA_FILE)
 
+    #Perulangan menu agar program terus berjalan sampai user memilih keluar
     while True:
         print("\n=== MENU DATA BARANG ===")
         print("1. Tampilkan semua barang")
@@ -169,8 +202,10 @@ def main():
         print("5. Simpan ke file")
         print("0. Keluar")
         
-        pilih = int(input("Pilihan menu : "))
+        #Mengambil input dari user
+        pilih = int(input("Pilihan menu : "))    
         
+        # Percabangan untuk memanggil fungsi yang sesuai
         if pilih == 1:
             tampilkan_barang(bukaData)
         elif pilih == 2:
@@ -184,9 +219,10 @@ def main():
             print("\nUpdate stok berhasil disimpan.")
         elif pilih == 0 :
             print("\nProgram Selesai.")
-            break
+            break      #Menghentikan loop 
         else: 
-            print("\nPilihan tidak valid. Coba lagi.")
+            print("\nPilihan tidak valid. Coba lagi.")   #Mengatasi error jika user memasukkan selain angka
+
             
-if __name__ == "__main__":
+if __name__ == "__main__":   #memastikan fungsi main() dipanggil
     main()
