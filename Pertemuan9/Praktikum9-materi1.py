5ef4af7b1bd99adb43f3c66330c65914f6d9ce94
jiwjie <mouziasyawalo@gmail.com>
2026-05-06 16:53:07 +0700

    Update Praktikum9-materi1.py
    pembahasannya ga kebaca di git

--- Pertemuan9/Praktikum9-materi1.py
+++ Pertemuan9/Praktikum9-materi1.py
@@ -24,5 +24,9 @@ print("Data pada root : ",root.data)
 print("Data child kiri : ",root.left)
 print("Data child kanan : ",root.right)
     
-    # pembahasan 
-    
+    # pembahasan dari kode
+''' Di kode ini merupakan langkah awal dalam membangun struktur pohon biner dengan cara 
+membentuk sebuah bernama class Node dahulu. Di sini, terdapat fungsi khusus init yang bertugas menyiapkan wadah 
+setiap kali sebuah titik baru dibuat, yaitu self.data sebagai tempat menyimpan nilai, serta self.left dan self.right yang berfungsi sebagai 
+penghubung ke "child" nanti. Lalu, dibentuk sebuah root yang memilki nilai "A". Disini kita baru membentuk root tanpa ada "child" dahulu, sehingga kedua penghubungnya kosong/bernilai None. 
+Jadi, inti dari proses ini adalah menyiapkan fondasi paling atas agar nantinya kita bisa menyambungkan data-data lain ke cabang kiri maupun kanan hingga membentuk satu kesatuan pohon yang utuh.
